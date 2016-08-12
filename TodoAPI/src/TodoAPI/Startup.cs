f84902286d7eefa49b6978a88891a4b1c009221b
Mike Liu <mike.liu.dev@gmail.com>
2016-08-12 11:37:07 -0400

    Hook up the Todo repository service.

--- TodoAPI/src/TodoAPI/Startup.cs
+++ TodoAPI/src/TodoAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TodoAPI.Models;
 
 namespace TodoAPI
 {
@@ -29,6 +30,9 @@ namespace TodoAPI
         {
             // Add framework services.
             services.AddMvc();
+
+            // Add the TodoRepository service.
+            services.AddSingleton<ITodoRepository, TodoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
